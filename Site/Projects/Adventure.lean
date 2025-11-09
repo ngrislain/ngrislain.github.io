2d80f6ac3071981b0e0d0801e583b789cf6478af
Nicolas Grislain <nicolas.grislain@gmail.com>
2025-11-09 09:37:14 +0100

    Reformulate

--- Site/Projects/Adventure.lean
+++ Site/Projects/Adventure.lean
@@ -11,17 +11,19 @@ date := { year := 2025, month := 11, day := 8 }
 
 ![Grisly Adventure](static/projects/adventure/sprites.png)
 
-I wanted a small playground to explore programming and 3d geometry with my kids, so we spun up
-a mini arcade project in [Pyxel](https://github.com/kitao/pyxel).
+My kids are between five and eleven, so I usually keep their screen time short—unless they are building the experience themselves. What started as a throwaway line quickly became a pact, and they held me to it. We pulled out sketchbooks, drew some ideas, and soon we were designing a small 3D game together.
+![Grisly Adventure](static/projects/adventure/design.jpeg)
+We eventually coded it in [Pyxel](https://github.com/kitao/pyxel), a very nice and simple Python framework.
+The goal was to build a playground where we could tinker with programming and 3D geometry side by side. Pyxel turned out to be perfect for spinning up a mini arcade project, so we leaned all the way in.
 
-It is called [Grisly Adventure](static/projects/adventure/adventure.html).
+Et Voilà! It is called [*Grisly Adventure*](static/projects/adventure/adventure.html).
+![Grisly Adventure](static/projects/adventure/play.jpeg)
+You can play it, just click on the [link](static/projects/adventure/adventure.html).
+Use the arrow keys to move and the space bar to shoot.
 
-You can play it, just click on the [link](static/projects/adventure/adventure.html),
-use the arrow keys to move and the space bar to shoot.
-
-Here is a screenshot of the game:
+Here are a few moments from the game:
 ![Grisly Adventure](static/projects/adventure/adventure_1.png)
-After a few trees burned:
+A few trees burned along the way:
 ![Grisly Adventure](static/projects/adventure/adventure_2.png)
-Oh no, the monsters are coming! 😱
+And of course, the monsters are coming! 😱
 ![Grisly Adventure](static/projects/adventure/adventure_3.png)
